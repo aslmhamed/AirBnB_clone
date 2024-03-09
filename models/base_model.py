a124b8185899e1852cf128dc1727c17e1a639524
Sufi-to <3thomas225@gmail.com>
2024-03-10 01:46:55 +0400

    pycodestyle version

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,6 @@ from datetime import datetime
 import models
 import uuid
 
-format_t = "%Y-%m-%dT%H:%M:%S.%f"
-
 
 class BaseModel():
     """This is the basemodel upon which all classes will be formed from."""
@@ -18,15 +16,15 @@ class BaseModel():
                 if key != "__class__":
                     setattr(self, key, value)
             if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], format_t)
+                self.created_at = datetime.fromisoformat(kwargs["created_at"])
             if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], format_t)
+                self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-            models.storage.save()
+            #models.storage.save()
 
     def __str__(self):
         """Return a string representation of the instance."""
@@ -39,15 +37,8 @@ class BaseModel():
 
     def to_dict(self):
         """Convert the object to a dictionary."""
-        # new_dict = self.__dict__.copy()
-        # if "created_at" in new_dict:
-        #     new_dict["created_at"] = new_dict["created_at"]
-        # if "updated_at" in new_dict:
-        #     new_dict["updated_at"] = new_dict["updated_at"]
-        # new_dict["__class__"] = self.__class__.__name__
-        # return new_dict
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = obj_dict['created_at'].strftime(format_t)
-        obj_dict['updated_at'] = obj_dict['updated_at'].strftime(format_t)
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
         return obj_dict
